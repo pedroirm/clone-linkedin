c4411101e5d0301e4ef12f6708fc9a94d2836863
Pedro Icaro <pedrocaro35@gmail.com>
2020-12-03 15:11:01 -0300

    First commit

---
+++ src/components/MobileHeader/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container,ProfileCircle, SearchInput, MessageIcon } from './styles';
+const MobileHeader: React.FC = () => {
+  return (
+  <Container>
+      <ProfileCircle src="https://github.com/pedroirm.png" />
+      <SearchInput placeholder="Pesquisar" />
+      <MessageIcon />
+  </Container>
+    );
+}
+
+export default MobileHeader;
