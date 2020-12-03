488b33992b4c39b3e99153f79023a0ca2d688e1b
Pedro Icaro <pedrocaro35@gmail.com>
2020-12-03 17:42:25 -0300

    Finalizando

--- src/components/RightColumn/index.tsx
+++ src/components/RightColumn/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import TrendingPanel from './TrendingPanel';
+import LoadingtrendingPanel from "../Shimmer/LoadingtrendingPanel";
+import TrendingPanel from "./TrendingPanel";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const RightColumn: React.FC = () => {
+const RightColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="right-column">
-      <TrendingPanel />
-      <TrendingPanel />
+      {isLoading ? (
+       <LoadingtrendingPanel />
+      ) : (
+        <>
+          <TrendingPanel />
+          <TrendingPanel />
+        </>
+      )}
     </Container>
   );
 };
